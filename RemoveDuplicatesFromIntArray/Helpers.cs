ce5d8f65cd705a920308e56900b1e5f1dfa0a1f7
Pavel Veremeienko <pavel.veremeienko@gmail.com>
2023-08-18 15:15:12 +0300

    Add project files.

---
+++ RemoveDuplicatesFromIntArray/Helpers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicatesFromIntArray
+{
+    internal static class Helpers
+    {
+        public static int[] RemoveDuplicates(this int[] ints)
+        {
+            int arrayLength = 1;
+            for (int i = 1, currentNumber = ints[0]; i < ints.Length; i++)
+            {
+                if (ints[i] != currentNumber)
+                {
+                    currentNumber = ints[i];
+                    arrayLength++;
+                }
+            }
+
+            int[] result = new int[arrayLength];
+            result[0] = ints[0];
+
+            for (int i = 1, j = 0; i < ints.Length; i++)
+            {
+                if (ints[i] > result[j])
+                {
+                    j++;
+                    result[j] = ints[i];
+                }
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<int> RemoveDuplicatesEnumerable(this int[] ints)
+        {
+            int? number = null;
+            foreach (int i in ints)
+            {
+                if (number.HasValue && number.Value == i)
+                {
+                    continue;
+                }
+
+                number = i;
+                yield return i;
+            }
+        }
+    }
+}
---
+++ RemoveDuplicatesFromIntArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RemoveDuplicatesFromIntArray;
+
+Console.WriteLine("Hello, World!");
+
+int[] input = new[] { 1, 2, 3, 4, 4, 56 };
+
+Console.WriteLine("Input");
+
+foreach (int i in input)
+{
+    Console.Write(i + ", ");
+}
+Console.Write("\n");
+
+int[] result = input.RemoveDuplicates();
+
+Console.WriteLine("Output");
+
+foreach (int i in result)
+{
+    Console.Write(i + ", ");
+}
+Console.Write("\n");
+
+Console.WriteLine("Output Enumerable");
+
+foreach (int i in input.RemoveDuplicatesEnumerable())
+{
+    Console.Write(i + ", ");
+}
+Console.Write("\n");
